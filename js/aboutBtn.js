24a9378e76a0549296f74d980aed29d013b5fddc
Pavlov Vitalii <pva8884@gmail.com>
2025-07-23 21:50:49 +0200

    Обнорвления

--- js/aboutBtn.js
+++ js/aboutBtn.js
@@ -1,11 +1,3 @@
-// const button = document.getElementById("callme");
-// function handleClick() {
-//     alert("Пожалуйста подтвердите что вы хотите что бы мы вам перезвонили в течении дня");
-//     result = confirm("Вы согласны?");
-
-// }
-button.addEventListener("click", handleClick);
-
 document.getElementById("aboutBtn").onclick = function() {
     document.getElementById("aboutModal").style.display = "block";
 };
